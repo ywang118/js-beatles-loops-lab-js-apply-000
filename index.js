d40d666f1bdefa5c4dfca3577b2e6d1c9c2c8c79
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-07-18 23:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  for (let i =0 ; i< musicians.length; i++) {
-    return (`${musicians} plays ${instruments}`);
+  var list = [];
+    for (let i =0 ; i< musicians.length; i++) {
+    list.push(`${musicians} plays ${instruments}`);
   }
 }
